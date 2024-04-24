27c69078614335bfa59c0e95bf2ce7be0538e07f
EthanDCR <clinteth.dev@gmail.com>
2024-04-23 20:03:07 -0500

    basiclly finished, just need to fix bug that still runs code if input is invalid and also need to create a function that keeps track of the score.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // Rock Paper Scissors Project (no GUI yet)
 
-function userinput(x) {
+// Get user input
+
+function userinput() {
   x = prompt("Rock, Paper, or Scissors? ");
   if (x === "rock") {
     return "rock";
@@ -9,9 +11,11 @@ function userinput(x) {
   } else if (x === "scissors") {
     return "scissors";
   } else {
-    return "Invalid input";
+    console.log("Invalid input");
   }
 }
+
+// Get random cpu input
 
 function cpuinput() {
   let y = Math.random();
@@ -24,8 +28,32 @@ function cpuinput() {
   }
 }
 
-let cpu = cpuinput();
 let user = userinput();
+let cpu = cpuinput();
 
-console.log(cpu);
-console.log(user);
+function compare() {
+  if (user === cpu) {
+    return "tie";
+  } else if (user === "rock" && cpu === "paper") {
+    return "user";
+  } else if (user === "paper" && cpu === "rock") {
+    return "user";
+  } else if (user === "scissors" && cpu === "paper") {
+    return "user";
+  } else {
+    return "cpu";
+  }
+}
+
+function winner() {
+  let result = compare(user, cpu);
+  if (result === "user") {
+    console.log("You Win, the cpu chose " + cpu);
+  } else if (result === "cpu") {
+    console.log("You Lose, the cpu chose " + cpu);
+  } else {
+    console.log("Tie, the cpu chose " + cpu);
+  }
+}
+
+winner();
